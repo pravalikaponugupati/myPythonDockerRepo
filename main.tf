6f5a44b0285c2722cf14fe9ec57fe01d7f4fd7b8
pravalikaponugupati <103594167+pravalikaponugupati@users.noreply.github.com>
2024-05-22 15:56:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,19 +16,41 @@ admin_enabled            = true
 # Remove the "georeplication_locations" attribute
 }
 
+resource "azurerm_app_service_plan" "acr-plan" {
+name                = "acrplan
+location            = azurerm_resource_group.acr-rg.location
+resource_group_name = azurerm_resource_group.acr-rg.name
+sku {
+tier = "Free"
+size = "F1"
+}
+}
+
+data "azurerm_key_vault" "acr-vault" {
+  name                = "acrvault"
+  resource_group_name = "acrresourcegroup1"
+}
+
+output "key_vault_id" {
+  value = data.azurerm_key_vault.acr-vault.id
+}
 
 
-# resource "azurerm_app_service" "acr-app" {
-# name                = "acrwebapp"
-# location            = azurerm_resource_group.acr-rg.location
-# resource_group_name = azurerm_resource_group.acr-rg.name
-# app_service_plan_id = azurerm_app_service_plan.acr-rg.id
+resource "azurerm_app_service" "acr-app" {
+  name                = "acr-web-app"
+  location            = azurerm_resource_group.acr-rg.location
+  resource_group_name = azurerm_resource_group.acr-rg.name
+  app_service_plan_id = azurerm_app_service_plan.acr-plan.id
 
-# site_config {
-# always_on = true
-# }
+  site_config {
+    always_on        = true
+    linux_fx_version = "DOCKER|${data.azurerm_container_registry.acr.name}.azurecr.io/DOCKER_IMAGE_NAME:DOCKER_IMAGE_TAG"
+  }
 
-# app_settings = {
-# "WEBSITE_HTTPLOGGING_RETENTION_DAYS" = "7"
-# }
-# }
+  app_settings = {
+    "WEBSITE_HTTPLOGGING_RETENTION_DAYS" = "7"
+    "DOCKER_REGISTRY_SERVER_URL"         = "https://${data.azurerm_container_registry.acr.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME"    = "${data.azurerm_container_registry.acr.admin_username}"
+    "DOCKER_REGISTRY_SERVER_PASSWORD"    = "${data.azurerm_key_vault_secret.acr_password.value}"
+  }
+}
