7ce695dd9e44ed6413d11b4726a528b1118e7630
pravalikaponugupati <103594167+pravalikaponugupati@users.noreply.github.com>
2024-05-28 14:51:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,42 +17,42 @@ admin_enabled            = true
 }
 
 resource "azurerm_app_service_plan" "acr-plan" {
-    name                = "acrplan"
-    location            = azurerm_resource_group.acr-rg.location
-    resource_group_name = azurerm_resource_group.acr-rg.name
+#     name                = "acrplan"
+#     location            = azurerm_resource_group.acr-rg.location
+#     resource_group_name = azurerm_resource_group.acr-rg.name
 
-    sku {
-        tier = "Free"
-        size = "F1"
-    }
-}
+#     sku {
+#         tier = "Free"
+#         size = "F1"
+#     }
+# }
 
-data "azurerm_key_vault" "acr-vault" {
-    name                = "acrvault"
-    resource_group_name = "acrresourcegroup1"
-}
+# data "azurerm_key_vault" "acr-vault" {
+#     name                = "acrvault"
+#     resource_group_name = "acrresourcegroup1"
+# }
 
-output "key_vault_id" {
-    value = data.azurerm_key_vault.acr-vault.id
-}
+# output "key_vault_id" {
+#     value = data.azurerm_key_vault.acr-vault.id
+# }
 
-resource "azurerm_app_service" "acr-app" {
-    name                = "acr-web-app"
-    location            = azurerm_resource_group.acr-rg.location
-    resource_group_name = azurerm_resource_group.acr-rg.name
-    app_service_plan_id = azurerm_app_service_plan.acr-plan.id
+# resource "azurerm_app_service" "acr-app" {
+#     name                = "acr-web-app"
+#     location            = azurerm_resource_group.acr-rg.location
+#     resource_group_name = azurerm_resource_group.acr-rg.name
+#     app_service_plan_id = azurerm_app_service_plan.acr-plan.id
 
-    site_config {
-        always_on        = true
-        linux_fx_version = "DOCKER|${data.azurerm_container_registry.acr.name}.azurecr.io/DOCKER_IMAGE_NAME:DOCKER_IMAGE_TAG"
-    }
+#     site_config {
+#         always_on        = true
+#         linux_fx_version = "DOCKER|${data.azurerm_container_registry.acr.name}.azurecr.io/DOCKER_IMAGE_NAME:DOCKER_IMAGE_TAG"
+#     }
 
-    app_settings = {
-        "WEBSITE_HTTPLOGGING_RETENTION_DAYS" = "7"
-        "DOCKER_REGISTRY_SERVER_URL"         = "https://${data.azurerm_container_registry.acr.login_server}"
-        "DOCKER_REGISTRY_SERVER_USERNAME"    = "${data.azurerm_container_registry.acr.admin_username}"
-        "DOCKER_REGISTRY_SERVER_PASSWORD"    = "${data.azurerm_key_vault_secret.acr_password.value}"
-    }
-}
+#     app_settings = {
+#         "WEBSITE_HTTPLOGGING_RETENTION_DAYS" = "7"
+#         "DOCKER_REGISTRY_SERVER_URL"         = "https://${data.azurerm_container_registry.acr.login_server}"
+#         "DOCKER_REGISTRY_SERVER_USERNAME"    = "${data.azurerm_container_registry.acr.admin_username}"
+#         "DOCKER_REGISTRY_SERVER_PASSWORD"    = "${data.azurerm_key_vault_secret.acr_password.value}"
+#     }
+# }
     
   
